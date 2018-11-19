046aa84fa13999f5d47fb4590f54332e3efdaf00
daria <daria@3-Macbook-Pro-13.local>
2018-11-19 12:27:37 +0100

    Updates

---
+++ precache-manifest.11910de88342ae740acb0f7c10507bb3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/reactive-weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "8b4513398ce92b40693b",
+    "url": "/reactive-weather/static/css/main.be422516.chunk.css"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/reactive-weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "2a8ff720bf17c8d39978",
+    "url": "/reactive-weather/static/js/1.2a8ff720.chunk.js"
+  },
+  {
+    "revision": "8b6d6f913eb610fb089d",
+    "url": "/reactive-weather/static/js/runtime~main.8b6d6f91.js"
+  },
+  {
+    "revision": "8b4513398ce92b40693b",
+    "url": "/reactive-weather/static/js/main.8b451339.chunk.js"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/reactive-weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/reactive-weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/reactive-weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "2a8ff720bf17c8d39978",
+    "url": "/reactive-weather/static/css/1.c363a328.chunk.css"
+  },
+  {
+    "revision": "d1b3ee8be6e6a1c0de230100346bb420",
+    "url": "/reactive-weather/index.html"
+  }
+];
