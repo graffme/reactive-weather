ecb4d980550be616c91755f90853a472b30ef0d4
daria <daria@3-Macbook-Pro-13.local>
2018-11-19 11:57:18 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-class App extends Component {
+import WeatherTile from './components/WeatherTile/WeatherTile.js';
+
+const API_KEY = "abe5affb6861893994d8ed784c41108d";
+const api = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=52.4&lon=16.9&units=metric&cnt=5&appid=" + API_KEY;
+//[52.4083, 16.9335]
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(api)
+      .then(response => response.json())
+      .then(data => {
+        const forecast = data.list;
+        console.log(forecast);
+        this.setState({ weather: forecast })
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container main-container">
+        <h1>5-days weather forecast</h1>
+        <p>This is a simple wather app, using OpenWeatherMap API to display 5-days forecast.</p>
+        <WeatherTile forecast={this.state.weather} />
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/components/WeatherTile/WeatherTile.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './style.scss';
+
+export default function WeatherTile(props) {
+  const months = ["Jan", "Feb", "Mar",
+  "Apr", "May", "June", "July", "Aug", "Sept",
+  "Oct", "Nov", "Dec"];
+  const weekdays = ["Sunday","Monday", "Tuesday", "Wednesday",
+  "Thursday", "Friday", "Saturday"];
+
+  const myDate = new Date();
+  var date = myDate.getDate();
+  var month = myDate.getMonth();
+  var day  = myDate.getDay();
+
+  const forecastItems = props.forecast.map((dailyForecast, index) =>
+    <div className="col text-center" key={index}>
+      <div className="weather-tile">
+        <p className="weather-date">{weekdays[day+index] + " " + date + " " + months[month]}</p>
+
+        {dailyForecast.weather[0].id >= 200 && dailyForecast.weather[0].id < 300 ?
+          <i class="wi-storm-showers"></i>
+        : null}
+
+        {dailyForecast.weather[0].id >= 300 && dailyForecast.weather[0].id < 600 ?
+          <i class="wi-rain"></i>
+        : null}
+
+        {dailyForecast.weather[0].id >= 600 && dailyForecast.weather[0].id < 700 ?
+          <i class="wi wi-snow"></i>
+        : null}
+
+        {dailyForecast.weather[0].id >= 700 && dailyForecast.weather[0].id < 800 ?
+          <i class="wi wi-fog"></i>
+        : null}
+
+        {dailyForecast.weather[0].id == 800 ?
+          <i class="wi wi-day-sunny"></i>
+        : null}
+
+        {dailyForecast.weather[0].id > 800 ?
+          <i class="wi wi-cloud"></i>
+        : null}
+
+        <p className="temp">{dailyForecast.temp.day} °C</p>
+        <p className="temp temp-night">{dailyForecast.temp.night} °C</p>
+      </div>
+    </div>
+  );
+  return(
+    <div className="row">
+      {forecastItems}
+    </div>
+  );
+}
